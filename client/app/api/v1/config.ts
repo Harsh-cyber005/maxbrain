59b7698f4083564b6dfaada1a89d88f7dc4cd4f6
harshmax <117888618+Harsh-cyber005@users.noreply.github.com>
2024-12-29 22:32:52 +0530

    config.ts, route.ts, emailer.ts (7 files changed, 479 deletions(-))

--- client/app/api/v1/config.ts
+++
@@ -1,9 +0,0 @@
-export const JWT_SECRET:string = "harshlovesrandomgirl";
-export const MONGO_URL:string = "mongodb+srv://harshmax:DijY3KIcl25qefSD@cluster0.xx9nals.mongodb.net/maxbrain?retryWrites=true&w=majority&appName=Cluster0";
-export const GMAIL_PASSWORD:string = "jnxomuhfybjemqhp";
-export const GMAIL_FROM:string = "mrx.zttg@gmail.com";
-export let OTP:string = "";
-
-export function changeOTP(newOTP: string): void {
-    OTP = newOTP;
-}
